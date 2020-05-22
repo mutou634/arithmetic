e65b048d16cae781ac633532d818de0d015eda3d
mutou634 <mutou634@163.com>
2020-05-22 23:29:16 +0800

    需修改

---
+++ 最长公共前缀.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String getStr="";
+        for(int i=1;i<strs.length;i++){
+            getStr=strs[0].substring(0,i);
+            for(String strInfo:strs){
+                if(!getStr.substring(0,i).equals(strInfo.substring(0,i))){
+                    return getStr;
+                }
+            }
+
+        }
+        return getStr;
+    }
+}
