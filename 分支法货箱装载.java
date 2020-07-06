0610d825b32f8137d5ce92956c29a773d9165a60
mutou634 <mutou634@163.com>
2020-07-06 23:45:04 +0800

    疑似规范写法

--- 分支法货箱装载.java
+++ 分支法货箱装载.java
@@ -1,103 +1,148 @@
-#include<iostream>
-using namespace std;
- 
-template <class T>
-class Queue
-{
-	public:
-		Queue(int MaxQueueSize=50);
-		~Queue(){delete [] queue;}
-		bool IsEmpty()const{return front==rear;}
-		bool IsFull(){return ( (  (rear+1)  %MaxSize==front )?1:0);}
-		T Top() const;
-		T Last() const;
-		Queue<T>& Add(const T& x);
-		Queue<T>& AddLeft(const T& x);
-		Queue<T>& Delete(T &x);
-		void Output(ostream& out)const;
-		int Length(){return (rear-front);}
-	private:
-		int front;
-		int rear;
-		int MaxSize;
-		T *queue;
-};
- 
-template<class T>
-Queue<T>::Queue(int MaxQueueSize)
-{
-	MaxSize=MaxQueueSize+1;
-	queue=new T[MaxSize];
-	front=rear=0;
-}
- 
-template<class T >
-T Queue<T>::Top()const
-{
-	if(IsEmpty())
-	{
-		cout<<"queue:no element,no!"<<endl;
-		return 0;
-	}
-	else return queue[(front+1) % MaxSize];
-}
- 
-template<class T>
-T Queue<T> ::Last()const
-{
-	if(IsEmpty())
-	{
-		cout<<"queue:no element"<<endl;
-		return 0;
-	}
-	else return queue[rear];
-}
- 
-template<class T>
-Queue<T>&  Queue<T>::Add(const T& x)
-{
-	if(IsFull())cout<<"queue:no memory"<<endl;
-	else
-	{
-		rear=(rear+1)% MaxSize;
-	    queue[rear]=x;
-	}
-	return *this;
-}
- 
-template<class T>
-Queue<T>&  Queue<T>::AddLeft(const T& x)
-{
-	if(IsFull())cout<<"queue:no memory"<<endl;
-	else
-	{
-		front=(front+MaxSize-1)% MaxSize;
-		queue[(front+1)% MaxSize]=x;
-	}
-	return *this;
-}
- 
-template<class T>
-Queue<T>&  Queue<T> ::Delete(T & x)
-{
-	if(IsEmpty())cout<<"queue:no element(delete)"<<endl;
-	else 
-	{
-		front=(front+1) % MaxSize;
-		x=queue[front];
-	}
-	return *this;
-}
- 
- 
-template<class T>
-void Queue <T>::Output(ostream& out)const
-{
-	for(int i=rear%MaxSize;i>=(front+1)%MaxSize;i--)
-	   out<<queue[i];
-}
- 
-template<class T>
-ostream& operator << (ostream& out,const Queue<T>& x)
-{x.Output(out);return out;
-}
+package arithmeticDemo;
+
+public class Container {
+		 public static void main(String[] args) {
+		  // n个货箱
+		  int n = 3;
+		  // 第一艘船的载重量
+		  float c1 = 50;
+		  // 第二艘船的载重量
+		  float c2 = 50;
+		  // 货箱质量数组
+		  float[] w = { 0, 10, 40, 40 };
+
+		  // 测试例子
+		  Container bfis = new Container(n, c1, c2, w);
+		  // 所有货箱的重量之和
+		  float s = bfis.getS();
+		  if (s <= c1 || s <= c2) {
+		   System.out.println("need only one ship!");
+		  }
+		  if (s > c1 + c2) {
+		   System.out.println("no solution!");
+		   return;
+		  }
+
+		  bfis.maxLoading(c1);
+
+		  float bestw = bfis.getBestw();
+
+		  if (s - bestw <= c2) {
+		   System.out.println("The first ship loading " + bestw);
+		   System.out.println("The second ship loading " + (s - bestw));
+		  } else {
+		   System.out.println("no solution!");
+		  }
+
+		 }
+
+		 private int n; // n个货箱
+		 private float c1; // 第一艘船的载重量
+		 private float c2; // 第二艘船的载重量
+		 private float bestw; // 第一艘船的最大装载
+		 private float ew = 0; // 当前船的装载量
+		 private float[] w; // 货箱质量数组
+		 private float s = 0; // 所有货箱的重量之和
+		 private MyQueue mq = new MyQueue(); // FIFO队列
+
+		 /**
+		  * 构造方法
+		  */
+		 public Container(int _n, float _c1, float _c2, float[] _w) {
+		  n = _n;
+		  c1 = _c1;
+		  c2 = _c2;
+		  w = _w;
+
+		  for (float f : _w) {
+		   s += f;
+		  }
+		 }
+
+		 /**
+		  * 最优装载值
+		  */
+		 public float maxLoading(float c) {
+		  mq.put(new Float(-1)); // 初始化结点队列，标记分层
+		  int i = 1; // E-结点的层
+		  ew = 0; // 当前船的装载量
+		  bestw = 0; // 目前的最优值
+
+		  while (!mq.empty()) { // 搜索子集空间树
+		   if (ew + w[i] <= c) { // 检查E-结点的左孩子，货箱i是否可以装载
+		    addLiveNode(ew + w[i], i); // 货箱i可以装载
+		   }
+
+		   addLiveNode(ew, i); // 右孩子总是可行的，不装载货物i
+
+		   ew = (Float) mq.get(); // 取下一个结点
+
+		   if (ew == -1) { // 到达层的尾部
+		    if (mq.empty()) {
+		     return bestw;
+		    }
+		    mq.put(new Float(-1));
+		    ew = (Float) mq.get(); // 取下一个结点
+		    i++; // ew的层
+		   }
+		  }
+		  return bestw;
+		 }
+		 public void addLiveNode(float wt, int i) {
+		  if (i == n) { // 是叶子
+		   if (wt > bestw) {
+		    bestw = wt;
+		   }
+		  } else { // 不是叶子
+		   mq.put(new Float(wt));
+		  }
+		 }
+
+		 public int getN() {
+		  return n;
+		 }
+
+		 public void setN(int n) {
+		  this.n = n;
+		 }
+
+		 public float getC1() {
+		  return c1;
+		 }
+
+		 public void setC1(float c1) {
+		  this.c1 = c1;
+		 }
+
+		 public float getC2() {
+		  return c2;
+		 }
+
+		 public void setC2(float c2) {
+		  this.c2 = c2;
+		 }
+
+		 public float getBestw() {
+		  return bestw;
+		 }
+
+		 public void setBestw(float bestw) {
+		  this.bestw = bestw;
+		 }
+
+		 public float getEw() {
+		  return ew;
+		 }
+
+		 public void setEw(float ew) {
+		  this.ew = ew;
+		 }
+
+		 public float getS() {
+		  return s;
+		 }
+
+		 public void setS(float s) {
+		  this.s = s;
+		 }
+}  
