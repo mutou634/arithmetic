5a9a00959f993e76f153459e1b99b9e6b041fd2f
mutou634 <mutou634@163.com>
2020-07-27 22:45:25 +0800

    哈希表结点

---
+++ 哈希表.java
@@ -0,0 +1,26 @@
+public class Info {
+ 
+    private String key;
+    private String data;
+ 
+    public Info(String key,String data){
+        this.key = key;
+        this.data = data;
+    }
+ 
+    public String getKey() {
+        return key;
+    }
+ 
+    public void setKey(String key) {
+        this.key = key;
+    }
+ 
+    public String getData() {
+        return data;
+    }
+ 
+    public void setData(String data) {
+        this.data = data;
+    }
+}
