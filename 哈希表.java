f6fb36f39797b7289c4a29368689d8f40e580fd8
mutou634 <mutou634@163.com>
2020-08-07 23:41:43 +0800

    哈希表的数据结构

--- 哈希表.java
+++ 哈希表.java
@@ -1,26 +1,25 @@
-public class Info {
- 
-    private String key;
-    private String data;
- 
-    public Info(String key,String data){
-        this.key = key;
-        this.data = data;
-    }
- 
-    public String getKey() {
-        return key;
-    }
- 
-    public void setKey(String key) {
-        this.key = key;
-    }
- 
-    public String getData() {
-        return data;
-    }
- 
-    public void setData(String data) {
-        this.data = data;
-    }
-}
+#include<stdlib.h>  
+#include<math.h>  
+struct HashTable;  
+struct ListNote;  
+typedef struct HashTable *HashTbl;  
+typedef struct ListNote *Position;  
+typedef Position List;  
+int Hash(int key,int tablesize);  
+int NextPrime(int x);  
+HashTbl InitalizeTable(int TableSize);  
+void DestroyTable(HashTbl H);  
+Position Find(int key,HashTbl H);  
+void Insert(int key, HashTbl H);  
+void Delete(int key,HashTbl H);  
+struct HashTable{   
+    int TableSize;  
+    Position *TheList;  
+};  
+struct ListNote{  
+    int element;  
+    Position next;  
+};  
+int Hash(int key,int tablesize){  
+    return key%tablesize;  
+}  
